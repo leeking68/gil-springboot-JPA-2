164f1f93d53e0c99e910e9cfd533a052e3cb08a5
gilhoLee <leeeeeegilho@gmail.com>
2021-07-31 20:15:24 +0900

    Initial commit

---
+++ src/main/java/order/restaurant/RestaurantApplication.java
@@ -0,0 +1,13 @@
+package order.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
